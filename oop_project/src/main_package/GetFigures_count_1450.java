68a6885b80e1319292117c6124ababdcff52537d
Nguyễn Đại <112848222+dainn67@users.noreply.github.com>
2023-05-25 01:54:05 +0700

    get figures' dynasty they belong to

--- oop_project/src/main_package/GetFigures_count_1450.java
+++ oop_project/src/main_package/GetFigures_count_1450.java
@@ -16,113 +16,59 @@ import org.jsoup.select.Elements;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-enum ContentType{
+enum ContentType {
 	TABLE, PARAGRAPH
 }
 
-public class CharacterScraper {
+public class NguoiKeSuScraper {
 	public static int count = 0;
-	public static int realCount = 0;
-
+	public static int actualCount = 0;
 	static int urlCounter = 0;
 
-	static String[] toSaveToFile = new String[155];
-	static String[] resDynastys = new String[155];
-
 	public static void main(String[] args) {
-		// DEBUGGING
-		debug();
+		try {
 
-		// RUN REAL MAIN FUNCTION
-//		runMain();
+			String url;
+			Document doc;
 
-//			encodeDynasty();
-//			decodeDynasty();
+			while (true) {
+//				if (urlCounter > 1450) break;
+				if (urlCounter < 175) {
+					urlCounter += 5;
+					continue;
+				} else if (urlCounter > 200)
+					break;
+
+				if (urlCounter == 0)
+					url = "https://nguoikesu.com/nhan-vat";
+				else
+					url = "https://nguoikesu.com/nhan-vat?start=" + urlCounter;
+				doc = Jsoup.connect(url).get();
+				getFigures(doc);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
 		System.out.println("\nCount: " + count);
-	}
-
-	static void debug() {
-		try {
-			while (true) {
-				if (urlCounter > 30)
-					break;
-				String url;
-				Document doc;
-				if (urlCounter == 0) {
-					url = "https://nguoikesu.com/nhan-vat";
-					doc = Jsoup.connect(url).get();
-					getFigures(doc);
-				} else {
-					url = "https://nguoikesu.com/nhan-vat?start=" + urlCounter;
-					doc = Jsoup.connect(url).get();
-					getFigures(doc);
-				}
-
-			}
-//			accessDetail("/nhan-vat/duong-tam-kha");
-//			accessDetail("/nhan-vat/duong-quy-phi");
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	static void runMain() {
-		try {
-//			Document document = Jsoup.connect("https://nguoikesu.com/nhan-vat").get();
-//			getFigures(document);
-//			System.out.println(normalizeString(document.toString()));
-
-			while (true) {
-				if (urlCounter > 1450)
-					break;
-				String url;
-				Document doc;
-				if (urlCounter == 0) {
-					url = "https://nguoikesu.com/nhan-vat";
-					doc = Jsoup.connect(url).get();
-					getFigures(doc);
-				} else {
-					url = "https://nguoikesu.com/nhan-vat?start=" + urlCounter;
-					doc = Jsoup.connect(url).get();
-					getFigures(doc);
-				}
-
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
 	}
 
 	static void getFigures(Document doc) {
 		Elements divs = doc.select("div.com-content-category-blog__item");
 
 		for (Element div : divs) {
-			Elements _figureName = div.select("h2");
-//			Elements _figureDesc = div.select("p");
+			Element figureNameElement = div.selectFirst("h2");
 			String _figureDetail = div.select("a").attr("href");
 
-			//get name
-			String nameContentString = "";
-			for (Element name : _figureName) {
-				nameContentString += name.text();
-			}
+			String figureName = figureNameElement.text();
 
-			//get outside description
-//			String descContentString = "";
-//			for (Element desc : _figureDesc) {
-//				descContentString += desc.text() + "";
-//			}
-//			descContentString += "\n";
-
-			System.out.println(normalizeString(nameContentString));
-//			System.out.println(normalizeString(descContentString));
+			System.out.println(normalizeString(figureName));
 
 			accessDetail(_figureDetail);
 
 			count++;
+			actualCount++;
 
 		}
 		urlCounter += 5;
@@ -130,102 +76,176 @@ public class CharacterScraper {
 
 	private static void accessDetail(String _figureDetail) {
 		ContentType type = ContentType.PARAGRAPH;
-		
+
 		String url = "https://nguoikesu.com" + _figureDetail;
 		try {
-//			System.out.println(url);
 			Document doc = Jsoup.connect(url).get();
-			Elements divs = doc.select("tr");
-			
-			if(divs != null) {
-				for(Element div : divs) {
-					Element subDivTitle = div.selectFirst("th");
-					Element subDivContent = div.selectFirst("td");
-					if(subDivTitle == null || subDivContent == null) continue;
-					
-					switch (normalizeString(subDivTitle.text())) {
-						case "Ten day du": {
-							type = ContentType.TABLE;
-							System.out.println("TEN KHAC: " + normalizeString(subDivContent.text().toString()) + "\n");
-							break;
-						}
-						case "Ten khac": {
-							type = ContentType.TABLE;
-							System.out.println("TEN KHAC: " + normalizeString(subDivContent.text().toString()) + "\n");
-							break;
-						}
-						case "Biet danh": {
-							type = ContentType.TABLE;
-							System.out.println("TEN KHAC: " + normalizeString(subDivContent.text().toString()) + "\n");
-							break;
-						}
-						case "Sinh": {
-							type = ContentType.TABLE;
-							System.out.println("SINH: " + normalizeString(subDivContent.text().toString()) + "\n");
-							break;
-						}
-						case "Mat": {
-							type = ContentType.TABLE;
-							System.out.println("MAT: " + normalizeString(subDivContent.text().toString()) + "\n");
-						}
-						default: break;
+			Elements rows = doc.select("tr");
+
+			if (rows != null) {
+				for (Element row : rows) {
+					Element subDivTitle = row.selectFirst("th");
+					Element subDivContent = row.selectFirst("td");
+
+					if (subDivTitle != null && normalizeString(subDivTitle.text()).equals("Dia ly")) {
+						System.out.println("	DIA DIEM");
+						return;
 					}
-				}
-				if(type.equals(ContentType.PARAGRAPH)) {
-					Element div = doc.selectFirst("div.com-content-article__body p");
-					Elements otherNameElements = div.select("b");
-					String otherName = "No other name";
-					
-					int i=0;
-					for(Element name: otherNameElements) {
-						if(i == 0) {
-							i++;
-							continue;
-						}
-						otherName = normalizeString(name.text());
+
+					String content = "";
+					if (subDivContent != null)
+						content = normalizeString(subDivContent.text());
+					else
+						continue;
+
+					if (subDivTitle == null || subDivContent == null)
+						continue;
+
+					switch (normalizeString(subDivTitle.text())) {
+					case "Ten day du": {
+						type = ContentType.TABLE;
+						System.out.println("	TEN KHAC: " + content);
 						break;
 					}
-					System.out.println("TEN KHAC: " + normalizeString(otherName));
-					String descContent = normalizeString(div.text().toString());
-					getYears(descContent);
+					case "Ten khac": {
+						type = ContentType.TABLE;
+						System.out.println("	TEN KHAC: " + content);
+						break;
+					}
+					case "Biet danh": {
+						type = ContentType.TABLE;
+						System.out.println("	TEN KHAC: " + content);
+						break;
+					}
+					case "Sinh": {
+						type = ContentType.TABLE;
+						System.out.println("	SINH: " + content);
+						break;
+					}
+					case "Mat": {
+						type = ContentType.TABLE;
+						System.out.println("	MAT: " + content);
+						break;
+					}
+					case "Than phu": {
+						type = ContentType.TABLE;
+						System.out.println("	THAN PHU: " + content);
+						break;
+					}
+					case "Than mau": {
+						type = ContentType.TABLE;
+						System.out.println("	THAN MAU: " + content);
+						break;
+					}
+					default:
+						break;
+					}
 				}
-			}else {
-				Element div = doc.selectFirst("div.com-content-article__body p");
-				System.out.println(div);
+
+				// if there are no content table
+				if (type.equals(ContentType.PARAGRAPH)) {
+
+					count--;
+//					int i=0;
+//					if(i == 0) return;
+
+					Element div = doc.selectFirst("div.com-content-article__body p");
+					
+					if(div != null) getDataFromParagraph(div);
+					else System.out.println("	NO PARAGRAPH, NO NOTHING ...");
+					
+				}
+			} else {
+				System.out.println("THERE ARE NO TR TAGS");
 			}
 		} catch (IOException e) {
-			System.out.println("ERROR");
+			System.out.println("ACCESSING DETAILS ERROR");
 			e.printStackTrace();
 		}
 	}
+	
+	static void getDataFromParagraph(Element div) {
+		Elements otherNameElements = div.select("b");
+		String otherName = "Khong co";
+
+		//1st bold text is real name -> 2nd bold text is otherName
+		if (otherNameElements.size() >= 2 && otherNameElements.get(1) != null)
+			otherName = normalizeString(otherNameElements.get(1).text());
+		
+		System.out.println("	TEN KHAC: " + otherName);
+		String descContent = normalizeString(div.text().toString());
+		
+		//get other data
+		getYears(descContent);
+		getHome(descContent);
+//		getParents(descContent);
+	}
+	
+	static void getHome(String text) {
+		Pattern pattern = Pattern.compile("nha\\s+([A-Z][a-z]*(\\s+[A-Z][a-z]*)*)");
+        Matcher matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            String name = matcher.group(1);
+            System.out.println("	Thoi: " + name);
+            return;
+        }
+        
+        pattern = Pattern.compile("trieu\\s+([A-Z][a-z]*(\\s+[A-Z][a-z]*)*)");
+        matcher = pattern.matcher(text);
+
+        if (matcher.find()) {
+            String name = matcher.group(1);
+            System.out.println("	Thoi: " + name);
+            return;
+        }
+        
+        System.out.println("	Thoi: Khong ro");
+
+	}
 
 	static void getYears(String str) {
-
-		// check the format: 1990-1991
-		Pattern pattern = Pattern.compile("(\\d{4})-(\\d{4})");
+		int birthYear = 0, deathYear = 0;
+		String birthYearString, deathYearString;
+		
+		// check the format: sinh nam 1990, mat nam 1990
+		Pattern pattern = Pattern.compile("(sinh|mat) nam (\\d{4})");
 		Matcher matcher = pattern.matcher(str);
 		if (matcher.find()) {
-			String birthYearString = matcher.group(1);
-			String deathYearString = matcher.group(2);
-			int birthYear1 = Integer.parseInt(birthYearString);
-			int deathYear1 = Integer.parseInt(deathYearString);
-			System.out.println("SINH (1): " + birthYear1);
-			System.out.println("MAT (1): " + deathYear1 + "}\n\n\n");
+			String yearString = matcher.group(2);
+			int year = Integer.parseInt(yearString);
+			System.out.println("	Sinh|Mat nam (3):" + year);
+			return;
+		}
+
+		// check the format: 1990-1991
+		pattern = Pattern.compile("(\\d{4})-(\\d{4})");
+		matcher = pattern.matcher(str);
+		if (matcher.find()) {
+			birthYearString = matcher.group(1);
+			deathYearString = matcher.group(2);
+
+			birthYear = Integer.parseInt(birthYearString);
+			deathYear = Integer.parseInt(deathYearString);
+
+			System.out.println("	SINH (1): " + birthYear);
+			System.out.println("	MAT (1): " + deathYear);
 			return;
 		}
 
 		pattern = Pattern.compile("(\\d{3})-(\\d{3})");
 		matcher = pattern.matcher(str);
 		if (matcher.find()) {
-			String birthYearString = matcher.group(1);
-			String deathYearString = matcher.group(2);
-			int birthYear1 = Integer.parseInt(birthYearString);
-			int deathYear1 = Integer.parseInt(deathYearString);
-			System.out.println("SINH (2): " + birthYear1);
-			System.out.println("MAT (2): " + deathYear1 + "}\n\n\n");
+			birthYearString = matcher.group(1);
+			deathYearString = matcher.group(2);
+
+			birthYear = Integer.parseInt(birthYearString);
+			deathYear = Integer.parseInt(deathYearString);
+
+			System.out.println("	SINH (2): " + birthYear);
+			System.out.println("	MAT (2): " + deathYear);
 			return;
 		}
-//		System.out.println("No format year-year");
 
 		// check the format: sinh nam 1990, mat nam 1990
 		pattern = Pattern.compile("(sinh|mat) nam (\\d{4})");
@@ -233,16 +253,11 @@ public class CharacterScraper {
 		if (matcher.find()) {
 			String yearString = matcher.group(2);
 			int year = Integer.parseInt(yearString);
-			System.out.println("Sinh|Mat nam (3):" + year + "}\n\n\n");
+			System.out.println("	Sinh|Mat nam (3):" + year);
 			return;
 		}
-//		System.out.println("No format sinh|mat nam");
 
 		// Check the format: "year-?" or "?-year"
-		// Initialize variables to hold the birth and death year
-		int birthYear = 0;
-		int deathYear = 0;
-
 		// First, check the 4 digit years
 		pattern = Pattern.compile("(\\d{1,4}|\\?)-?(\\d{1,4}|\\?)?");
 		matcher = pattern.matcher(str);
@@ -253,22 +268,24 @@ public class CharacterScraper {
 			}
 			if (match.contains("-")) {
 				String[] years = match.split("-");
-				birthYear = Integer.parseInt(years[0]);
-				deathYear = Integer.parseInt(years[1]);
+				if (years[0] != null)
+					birthYear = Integer.parseInt(years[0]);
+				if (years[1] != null)
+					deathYear = Integer.parseInt(years[1]);
 			} else {
 				birthYear = Integer.parseInt(match);
 			}
 		}
 
-		// Print the birth and death year
 		if (birthYear != 0 || deathYear != 0) {
-			if(deathYear < 100) deathYear = 0;
-			if(birthYear < 100) birthYear = 0;
-			System.out.println("SINH (4): " + birthYear);
-			System.out.println("MAT (4): " + deathYear + "}\n\n\n");
+			if (deathYear < 100)
+				deathYear = 0;
+			if (birthYear < 100)
+				birthYear = 0;
+			System.out.println("	SINH (4): " + birthYear);
+			System.out.println("	MAT (4): " + deathYear);
 			return;
 		}
-//		System.out.println("No format ?-year, year-? 4 digits");
 
 		// Then check the 3 digit years
 		pattern = Pattern.compile("(\\d{1,3}|\\?)-?(\\d{1,3}|\\?)?");
@@ -280,29 +297,32 @@ public class CharacterScraper {
 			}
 			if (match.contains("-")) {
 				String[] years = match.split("-");
-				birthYear = Integer.parseInt(years[0]);
-				deathYear = Integer.parseInt(years[1]);
+				if (years[0] != null)
+					birthYear = Integer.parseInt(years[0]);
+				if (years[1] != null)
+					deathYear = Integer.parseInt(years[1]);
 			} else {
 				birthYear = Integer.parseInt(match);
 			}
 		}
 
-		// Print the birth and death year
 		if (birthYear != 0 || deathYear != 0) {
-			System.out.println("SINH (5): " + birthYear);
-			System.out.println("MAT(5): " + deathYear);
+			System.out.println("	SINH (5): " + birthYear);
+			System.out.println("	MAT(5): " + deathYear);
 			return;
 		}
 
-//		System.out.println("No format ?-year, year-? 3 digits");
+		System.out.println("	SINH (6): " + birthYear);
+		System.out.println("	MAT(6): " + deathYear);
+
 	}
-	
+
 	static void encodeDynasty() {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
-		String json = gson.toJson(toSaveToFile);
+//		String json = gson.toJson(toSaveToFile);
 
 		try (FileWriter writer = new FileWriter("data.json")) {
-			writer.write(json);
+//			writer.write(json);
 			System.out.println("Data saved to data.json");
 		} catch (IOException e) {
 			System.err.println("Failed to save data: " + e.getMessage());
@@ -326,15 +346,14 @@ public class CharacterScraper {
 
 			// Decode the JSON string into a String array
 			Gson gson = new Gson();
-			resDynastys = gson.fromJson(json, String[].class);
+//			resDynastys = gson.fromJson(json, String[].class);
 
 			// Print the decoded array to the console
-			for (String item : resDynastys) {
-				if (item != null) {
-					realCount++;
-					System.out.println(item);
-				}
-			}
+//			for (String item : resDynastys) {
+//				if (item != null) {
+//					System.out.println(item);
+//				}
+//			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
