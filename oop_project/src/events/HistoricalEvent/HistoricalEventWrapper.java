416d8c94334e55a0c1ff368cf23f3c5f7fde1574
tranky217 <trantrongky217@gmail.com>
2023-06-02 23:15:31 +0700

    adding events

---
+++ oop_project/src/events/HistoricalEvent/HistoricalEventWrapper.java
@@ -0,0 +1,8 @@
+package HistoricalEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoricalEventWrapper {
+    public final List<HistoricalEvent> eventList = new ArrayList<HistoricalEvent>();
+}
