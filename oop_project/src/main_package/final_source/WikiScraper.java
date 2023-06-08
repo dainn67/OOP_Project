538b8ffaa70a744c5d7001b6a21f089f035dc7b9
Nguyễn Đại <112848222+dainn67@users.noreply.github.com>
2023-06-09 01:25:19 +0700

    Add files via upload

--- oop_project/src/main_package/final_source/WikiScraper.java
+++ oop_project/src/main_package/final_source/WikiScraper.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -18,12 +20,14 @@ import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
 
 public class WikiScraper {
+
 	public static void main(String[] args) throws Exception {
 		List<King> kings = new ArrayList<King>();
 
 		String url = "https://vi.wikipedia.org/wiki/Vua_Vi%E1%BB%87t_Nam#Th%E1%BB%9Di_k%E1%BB%B3_chia_c%E1%BA%AFt";
 		Document doc = Jsoup.connect(url).get();
 
+		// there are 2 types of table
 		Elements tables = doc.select("div.mw-parser-output table:not(.wikitable)[width!=\"1400\"]");
 		Elements tables_wikitable = doc.select("div.mw-parser-output table.wikitable[width!=\"1400\"]");
 
@@ -31,18 +35,7 @@ public class WikiScraper {
 		loopTables(tables_wikitable, kings);
 
 		prtKingList(kings);
-//		encodeData(kings);
-//		decodeData();
 		System.out.println("Total kings: " + kings.size());
-	}
-
-	static void debug() {
-		String url = "https://vi.wikipedia.org/wiki/Vua_Vi%E1%BB%87t_Nam#Th%E1%BB%9Di_k%E1%BB%B3_chia_c%E1%BA%AFt";
-		try {
-			Document doc = Jsoup.connect(url).get();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 
 	static void loopTables(Elements tables, List<King> kings) {
@@ -77,7 +70,7 @@ public class WikiScraper {
 	}
 
 	static King getKing(Elements characterAttributes) {
-		String[] kingAttributes = new String[5];
+		String[] kingAttributes = new String[9];
 		for (int i = 0; i < kingAttributes.length; i++)
 			kingAttributes[i] = "Khong ro";
 
@@ -97,55 +90,128 @@ public class WikiScraper {
 		accessDetailKing(detailLink, kingAttributes);
 
 		return new King(ten, kingAttributes[0], kingAttributes[1], kingAttributes[2], kingAttributes[3],
-				kingAttributes[4], namTriVi);
+				kingAttributes[4], kingAttributes[5], kingAttributes[6], namTriVi, kingAttributes[8]);
 	}
 
 	static void accessDetailKing(String detailLink, String[] kingAttributes) {
-		// for each king in the tables, access the link inside to get more data
-		String url = "https://vi.wikipedia.org" + detailLink;
 		try {
+			// for each king in the tables, access the link inside to get more data
+			String url = "https://vi.wikipedia.org" + detailLink;
 			Document detailDoc = Jsoup.connect(url).get();
-			Element boxDetail = detailDoc.selectFirst("table.infobox");
-			if (boxDetail == null) {
-				return;
-			}
-			Elements trTags = boxDetail.select("tr");
-			Element title, content;
-			for (Element trTag : trTags) {
-				title = trTag.selectFirst("th");
-				content = trTag.selectFirst("td");
-				if (title == null || content == null)
-					continue;
-				String contentString = normalizeString(content.text()).replaceAll("\\[.*?\\]", "");
-				switch (normalizeString(title.text())) {
-				case "Sinh": {
-					kingAttributes[0] = contentString;
-					break;
-				}
-				case "Mat": {
-					kingAttributes[1] = contentString;
-					break;
-				}
-				case "Trieu dai": {
-					kingAttributes[2] = contentString;
-					break;
-				}
-				case "Than phu": {
-					kingAttributes[3] = contentString;
-					break;
-				}
-				case "Than mau": {
-					kingAttributes[4] = contentString;
-					break;
-				}
-				}
-			}
-
+			
+			getDataFromTable(detailDoc, kingAttributes);
+			getDataFromParagraph(detailDoc, kingAttributes);
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	static void getDataFromTable(Document detailDoc, String[] kingAttributes) {
+		Element boxDetail = detailDoc.selectFirst("table.infobox");
+		if (boxDetail == null)
+			return;
+		
+		Elements trTags = boxDetail.select("tr");
+		
+		Element title, content;
+		for (Element trTag : trTags) {
+			title = trTag.selectFirst("th");
+			content = trTag.selectFirst("td");
+			if (title == null || content == null)
+				continue;
+			
+			String contentString = normalizeString(content.text()).replaceAll("\\[.*?\\]", "");
+			switch (normalizeString(title.text())) {
+			case "Sinh": {
+				kingAttributes[1] = contentString;
+				break;
+			}
+			case "Mat": {
+				kingAttributes[2] = contentString;
+				break;
+			}
+			case "Than phu": {
+				kingAttributes[3] = contentString;
+				break;
+			}
+			case "Than mau": {
+				kingAttributes[4] = contentString;
+				break;
+			}
+			case "Trieu dai": {
+				kingAttributes[5] = contentString;
+				break;
+			}
+			}
+		}
 
+	}
+
+	static void getDataFromParagraph(Document doc, String[] kingAttributes) {
+		Elements pTags = doc.select("p");
+		StringBuilder desc = new StringBuilder();
+		if(pTags.size() > 0) {
+			for(Element pTag: pTags) {
+				desc.append(normalizeString(pTag.text())).append(" ");
+			}
+		}
+		
+		getDynasty(desc.toString(), kingAttributes);
+	}
+	
+
+	static void getDynasty(String text, String[] kingAttributes) {
+		if (text.contains("vua Le chua Trinh")) {
+			kingAttributes[5] = "Vua Le chua Trinh";
+			return;
+		}
+
+		Pattern pattern = Pattern.compile("nha\\s+([A-Z][a-z]*(\\s+[A-Z][a-z]*)*)");
+		Matcher matcher = pattern.matcher(text);
+
+		String dynasty, dynastyGroup;
+		if (matcher.find()) {
+			dynastyGroup = matcher.group(0);
+			dynasty = matcher.group(1);
+
+			// check if "Le so"
+			int index = text.indexOf(dynastyGroup);
+			String afterSubstring = text.substring(index + dynastyGroup.length());
+			if (afterSubstring.trim().startsWith("so")) {
+				dynasty += " so";
+			}
+			
+			kingAttributes[5] = dynasty;
+			return;
+		}
+
+		if (dynastyPattern(text, "trieu\\s+([A-Z][a-z]*(\\s+[A-Z][a-z]*)*)", kingAttributes))
+			return;
+		if (dynastyPattern(text, "trieu dai\\s+([A-Z][a-z]*(\\s+[A-Z][a-z]*)*)", kingAttributes))
+			return;
+		if (dynastyPattern(text, "Nha\\s+([A-Z][a-z]*(\\s+[A-Z][a-z]*)*)", kingAttributes))
+			return;
+	}
+
+	static Boolean dynastyPattern(String text, String patternString, String[] kingAttributes) {
+		Pattern pattern = Pattern.compile(patternString);
+		Matcher matcher = pattern.matcher(text);
+
+		if (matcher.find()) {
+			kingAttributes[5] = matcher.group(1);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	
+	
+	//TODO: handle the duplicate functions in 2 files, apply getHome and getParent for this file
+	
+	
+	
 	static void encodeData(List<King> kings) {
 		Gson gson = new GsonBuilder().setPrettyPrinting().create();
 		String json = gson.toJson(kings);
@@ -202,9 +268,11 @@ public class WikiScraper {
 			System.out.println("    Sinh: " + king.getBornYear());
 			System.out.println("    Mat: " + king.getDeathYear());
 			System.out.println("    Trieu dai: " + king.getDynasty());
+			System.out.println("    Que quan: " + king.getHome());
 			System.out.println("    Cha: " + king.getFather());
 			System.out.println("    Me: " + king.getMother());
-			System.out.println("    Nam Tri Vi: " + king.getNamTriVi() + "\n");
+			System.out.println("    Mo ta: " + king.getDesc());
+			System.out.println("    Nam Tri Vi: " + king.getKingYear() + "\n");
 		}
 	}
 
