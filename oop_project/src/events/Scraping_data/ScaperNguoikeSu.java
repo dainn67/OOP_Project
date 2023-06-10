74acde638e42753ecfd644b06922cf2d2066b10f
tranky217 <trantrongky217@gmail.com>
2023-06-10 15:17:03 +0700

    change structure

---
+++ oop_project/src/events/Scraping_data/ScaperNguoikeSu.java
@@ -0,0 +1,14 @@
+import Scraping_data.ScrapeUtil;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+
+public class ScaperNguoiKeSu {
+    public static void main(String[] args) throws IOException {
+        String base = "https://nguoikesu.com";
+        ScrapeUtil.EventScrapper("https://nguoikesu.com/tu-lieu/quan-su?filter_tag[0]=", base);
+//        ScrapeUtil.handleDetail("https://nguoikesu.com/tu-?lieu/quan-su/chien-tranh-nguyen-mong-dai-viet-lan-2");
+    }
+}
