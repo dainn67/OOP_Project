a6b56d8de7b75c3c16279e9b021e8ed573cd3549
NguyenDinhHongPhongBK <101378294+NguyenDinhHongPhongBK@users.noreply.github.com>
2023-07-01 16:08:22 +0700

    Add files via upload

---
+++ oop_project/src/festival/FinalParameter.java
@@ -0,0 +1,6 @@
+package festival;
+
+public class FinalParameter {
+	public static final String FIGURE_FILE_ADDRESS="E:/figures.json"; 
+	public static final String FESTIVAL_FILE_ADDRESS="E:/festival11.json"; 
+}
