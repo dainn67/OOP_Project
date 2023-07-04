fb77e0a4a4da8786ec62f2e481dd2c043bb124a7
NguyenDinhHongPhongBK <101378294+NguyenDinhHongPhongBK@users.noreply.github.com>
2023-07-04 22:49:47 +0700

    Add files via upload

---
+++ oop_project/src/relic/Object/Province.java
@@ -0,0 +1,12 @@
+package relic.Object;
+
+public class Province {
+    private String province;
+
+    public void setProvince(String province) {
+        this.province = province;
+    }
+    public String getProvince() {
+    	return this.province;
+    }
+}
---
+++ oop_project/src/relic/Object/Relic.java
@@ -0,0 +1,60 @@
+package relic.Object;
+
+import java.util.ArrayList;
+
+import Figures.Figure;
+
+public class Relic extends Type {
+    private String name;
+    private String location;
+    private String time;
+    private String desc;
+    private String id;
+    private ArrayList<Figure> figures = new ArrayList<Figure>();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public void addFigure(Figure figure) {
+        this.figures.add(figure);
+    }
+
+    public String getID() {
+        return this.id;
+    }
+
+    public String getDesc() {
+        return this.desc;
+    }
+    
+    public Relic() {
+		super();
+	}
+    @Override
+    public String toString() {
+    	String strRelic = new String();
+    	strRelic += "Name: " + this.name +"\n";
+    	strRelic += "ID: " + this.id +"\n";
+    	strRelic += "Location: " + this.location +"\n";
+    	strRelic += "Time: " + this.time +"\n";
+    	strRelic += "Description: " + this.desc +"\n";
+    	return strRelic;
+    }
+}
